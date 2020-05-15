7bd272b65fd4f9db7ae2ee103d3934b3dbab1d57
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-16 00:07:49 +0100

    initial commit

---
+++ src/home/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div>
+      <img src="../assets/robot-home.png" alt="robot" aria-hidden="true" class="robot" />
+    </div>
+    <div class="get-started">
+      <a href>Get started</a> build your first robot
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+a {
+  color: #42b983;
+}
+</style>
