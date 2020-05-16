b506c2c525f8654732efb220e10e65a100997e8c
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-16 07:32:04 +0100

    add images from parts data

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -3,31 +3,31 @@
     <div>
       <div class="top-row">
         <div class="top part">
-          <img src="./images/head-big-eye.png" title="head" />
+          <img v-bind:src="availableParts.heads[0].src" title="head" />
           <button class="prev-selector">&#9668;</button>
-          <button class="next-selector">&#9658;</button>
+          <button @click="selectNextHead()" class="next-selector">&#9658;</button>
         </div>
       </div>
       <div class="middle-row">
         <div class="left part">
-          <img src="./images/arm-articulated-claw.png" title="left arm" />
+          <img v-bind:src="availableParts.arms[0].src" title="left arm" />
           <button class="prev-selector">&#9650;</button>
           <button class="next-selector">&#9660;</button>
         </div>
         <div class="center part">
-          <img src="./images/torso-flexible-gauged.png" title="left arm" />
+          <img v-bind:src="availableParts.torsos[0].src" title="left arm" />
           <button class="prev-selector">&#9668;</button>
           <button class="next-selector">&#9658;</button>
         </div>
         <div class="right part">
-          <img src="./images/arm-dual-claw.png" title="left arm" />
+          <img v-bind:src="availableParts.arms[0].src" title="left arm" />
           <button class="prev-selector">&#9650;</button>
           <button class="next-selector">&#9660;</button>
         </div>
       </div>
       <div class="bottom-row">
         <div class="bottom part">
-          <img src="./images/base-single-wheel.png" title="left arm" />
+          <img v-bind:src="availableParts.bases[0].src" title="left arm" />
           <button class="prev-selector">&#9668;</button>
           <button class="next-selector">&#9658;</button>
         </div>
@@ -37,29 +37,38 @@
 </template>
 
 <script>
-export default {};
+import availableParts from "../data/parts";
+export default {
+  name: "RobotBuilder",
+  data() {
+    return {
+      availableParts
+    };
+  },
+  methods: {}
+};
 </script>
 
 <style scoped>
 .part {
   position: relative;
-  width:165px;
-  height:165px;
+  width: 165px;
+  height: 165px;
   border: 3px solid #aaa;
-} 
+}
 .part img {
-  width:165px;
+  width: 165px;
 }
 .top-row {
-  display:flex;
+  display: flex;
   justify-content: space-around;
 }
 .middle-row {
-  display:flex;
+  display: flex;
   justify-content: center;
 }
 .bottom-row {
-  display:flex;
+  display: flex;
   justify-content: space-around;
   border-top: none;
 }
@@ -83,7 +92,7 @@ export default {};
 }
 .prev-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   left: -28px;
   width: 25px;
@@ -91,14 +100,15 @@ export default {};
 }
 .next-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   right: -28px;
   width: 25px;
   height: 171px;
 }
-.center .prev-selector, .center .next-selector {
-  opacity:0.8;
+.center .prev-selector,
+.center .next-selector {
+  opacity: 0.8;
 }
 .left .prev-selector {
   top: -28px;
@@ -109,20 +119,20 @@ export default {};
 .left .next-selector {
   top: auto;
   bottom: -28px;
-  left: -3px;    
+  left: -3px;
   width: 144px;
   height: 25px;
 }
 .right .prev-selector {
   top: -28px;
-  left: 24px;  
+  left: 24px;
   width: 144px;
   height: 25px;
 }
 .right .next-selector {
   top: auto;
   bottom: -28px;
-  left: 24px;    
+  left: 24px;
   width: 144px;
   height: 25px;
 }
